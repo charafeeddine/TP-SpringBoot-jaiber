7d89fa14977a8940d552873e2ff808dc42c3c72c
charafeeddine <charafeddin@Charafeddins-MacBook-Pro.local>
2023-01-31 14:53:49 +0100

    fixed some bugs

--- my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
+++ my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
@@ -2,7 +2,13 @@ package com.example.myfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableHystrixDashboard
 @SpringBootApplication
 public class MyFinderApplication {
 
